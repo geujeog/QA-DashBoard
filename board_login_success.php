0c773b684e4b4f6f3cefbc0ffc89f1c3446b913c
geujeog <72426016+geujeog@users.noreply.github.com>
2021-05-05 02:55:04 +0900

    Delete board_login_success.php

--- board_login_success.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<meta http-equiv="refresh" content="3; url=./board.php">
-</head>
-<body>
-<p id='result'> Success </p>
-<?php session_start(); echo $_SESSION['session_id']; ?>
-</body>
-</html>
